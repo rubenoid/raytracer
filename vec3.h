ea9ebb00312f6ab60f10fb1918617e4f119cb9a4
Ruben <rubenvanschaik@gmail.com>
2023-02-09 01:26:25 +0100

    8.3 Using Gamma Correction for Accurate Color Intensity

--- vec3.h
+++ vec3.h
@@ -11,6 +11,7 @@ public:
 	vec3() : e{0,0,0} {}
 	vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
+
 	double x() const { return e[0]; }
 	double y() const { return e[1]; }
 	double z() const { return e[2]; }
@@ -47,6 +48,14 @@ public:
 
 public:
 	double e[3];
+
+	inline static vec3 random() {
+		return vec3(random_double(), random_double(), random_double());
+	}
+
+	inline static vec3 random(double min, double max) {
+		return vec3(random_double(min,max), random_double(min, max), random_double(min,max));
+	}
 };
 
 // type aliases for vec3
@@ -95,6 +104,14 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
 	return v / v.length();
+}
+
+vec3 random_in_unit_sphere() {
+	while (true) {
+		auto p = vec3::random(-1,1);
+		if (p.length_squared() >= 1) continue;
+		return p;
+	}
 }
 
 #endif //RAYTRACER_VEC3_H
